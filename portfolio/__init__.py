3cd18320da8b9d31f4a926d1279f3b12c1bcb466
Matheus Felipe <mathfelpessoal@gmail.com>
2024-01-27 14:29:07 -0300

    Remoção de arquivos

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -26,29 +26,7 @@ projects = [
         "categories": ["Python"],
         "slug": "luzes",
         "prod": "https://github.com/MatheusFelipeLS/Jogo-de-memorizacao-de-cores",
-    },
-    {
-        "name": "Habit tracking app with Python and MongoDB",
-        "thumb": "img/habit-tracking.png",
-        "hero": "img/habit-tracking-hero.png",
-        "categories": ["python", "web"],
-        "slug": "habit-tracking",
-        "prod": "https://udemy.com",
-    },
-    {
-        "name": "Personal finance tracking app with React",
-        "thumb": "img/personal-finance.png",
-        "hero": "img/personal-finance.png",
-        "categories": ["react", "javascript"],
-        "slug": "personal-finance",
-    },
-    {
-        "name": "REST API Documentation with Postman and Swagger",
-        "thumb": "img/rest-api-docs.png",
-        "hero": "img/rest-api-docs.png",
-        "categories": ["writing"],
-        "slug": "api-docs",
-    },
+    }
 ]
 
 slug_to_project = {project["slug"]: project for project in projects}
